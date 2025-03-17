968cf1f8ab8e959bdf61fa28b7f7accb15e16514
vanshpaliwal13 <vanshpaliwal13@gmail.com>
2025-03-17 18:12:03 +0530

    Add files via upload

---
+++ codsoft task 1 .cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main() {
+    // Initialize random number generator
+    std::srand(std::time(0)); // use current time as seed for random generator
+    int random_number = std::rand() % 100 + 1; // random number between 1 and 100
+
+    int user_guess = 0;
+    int attempts = 0;
+
+    std::cout << "Welcome to the Guessing Game!\n";
+    std::cout << "I have selected a random number between 1 and 100.\n";
+    std::cout << "Try to guess the number!\n";
+
+    // Loop until the user guesses the correct number
+    do {
+        std::cout << "Enter your guess: ";
+        std::cin >> user_guess;
+        attempts++;
+
+        if (user_guess < random_number) {
+            std::cout << "Too low! Try again.\n";
+        } else if (user_guess > random_number) {
+            std::cout << "Too high! Try again.\n";
+        } else {
+            std::cout << "Congratulations! You guessed the correct number in " << attempts << " attempts.\n";
+        }
+    } while (user_guess != random_number);
+    1return 0;
+}
